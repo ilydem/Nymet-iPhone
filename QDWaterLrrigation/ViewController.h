9e76361c187eb0ef3576609729310a00fd475ebb
Mako <mako@MakomatoMac-mini.local>
2012-10-24 09:37:00 +0800

    Initial Commit

---
+++ QDWaterLrrigation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QDWaterLrrigation
+//
+//  Created by Mako on 12-10-24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ QDWaterLrrigation/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  QDWaterLrrigation
+//
+//  Created by Mako on 12-10-24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return YES;
+    }
+}
+
+@end
